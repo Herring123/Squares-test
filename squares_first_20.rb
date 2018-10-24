e47ef90b7f0004c6f5e9a3ed5eaa65fcf63c36ed
Ben Herring <ben_herring@hotmail.com>
2018-10-24 20:40:01 +0100

    added test and done

--- squares_first_20.rb
+++ squares_first_20.rb
@@ -1,6 +1,18 @@
 numbers = (1..20)
-test=numbers.each do |number|
+numbers.each do |number|
  puts number * number
 end
+
+
+
+# simple test
+# numbers = (1..20)
+# test=numbers.map do |number|
+#   number * number
+# end
+
+# puts test == [1,4,9,16,25,36,49,64,81,100,121,144,169,196,225,256,289,324,361,400]
+
+
 
 
